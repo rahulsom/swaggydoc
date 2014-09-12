435dcf11f70665446feed129808c21e3a5d013fa
Rahul Somasunderam <rahul.som@gmail.com>
2014-09-11 19:00:58 -0700

    Code is slightly more modular

--- grails-app/controllers/com/github/rahulsom/swaggydoc/ApiController.groovy
+++ grails-app/controllers/com/github/rahulsom/swaggydoc/ApiController.groovy
@@ -1,36 +1,67 @@
 package com.github.rahulsom.swaggydoc
 
-import com.wordnik.swagger.annotations.Api
-import com.wordnik.swagger.annotations.ApiImplicitParam
-import com.wordnik.swagger.annotations.ApiImplicitParams
-import com.wordnik.swagger.annotations.ApiOperation
-import com.wordnik.swagger.annotations.ApiResponses
+import com.wordnik.swagger.annotations.*
 import grails.converters.JSON
 import org.codehaus.groovy.grails.commons.GrailsClass
 
+import java.lang.reflect.AccessibleObject
 import java.lang.reflect.Field
+import java.lang.reflect.Method
 
 class ApiController {
 
+    /**
+     * Empty Method. Needed for rendering GSP as HTML.
+     */
     def index() {
     }
 
-    def resources() {
-        def documentedControllers = grailsApplication.controllerClasses.findAll { controller ->
-            getApi(controller)
-        }.sort { controller ->
-            getApi(controller).position()
-        }
+    /**
+     * Empty Method. Needed for rendering GSP as HTML.
+     */
+    def images() {
+        response.sendRedirect(g.resource(dir: 'images', file: 'throbber.gif').toString())
+    }
 
-        def apis = documentedControllers.collect { controller ->
-            def name = controller.logicalPropertyName
-            [
-                    path       : g.createLink(controller: 'api', action: 'show', id: name, absolute: true),
-                    description: getApi(controller).description() ?: controller.naturalName
-            ]
-        }
-        def config = grailsApplication.config.swaggydoc
-        def info = [
+    /**
+     * Renders the Swagger Resources.
+     * @return
+     */
+    def resources() {
+        def apis = grailsApplication.controllerClasses.
+                findAll { getApi(it) }.
+                sort { getApi(it).position() }.
+                collect { controllerToApi(it) }
+
+        ConfigObject config = grailsApplication.config.swaggydoc
+
+        render([
+                apiVersion    : config.apiVersion ?: grailsApplication.metadata['app.version'],
+                swaggerVersion: '1.2',
+                apis          : apis,
+                info          : infoObject
+        ] as JSON)
+    }
+
+    /**
+     * Converts a controller to an api declaration
+     * @param controller
+     */
+    private Map controllerToApi(GrailsClass controller) {
+        def name = controller.logicalPropertyName
+        [
+                path       : g.createLink(controller: 'api', action: 'show', id: name, absolute: true),
+                description: getApi(controller).description() ?: controller.naturalName
+        ]
+    }
+
+    /**
+     * Provides an Info Object for Swagger
+     * @return
+     */
+    private Map<String, Object> getInfoObject() {
+        ConfigObject config = grailsApplication.config.swaggydoc
+        [
                 contact          : config.contact,
                 description      : config.description,
                 license          : config.license,
@@ -40,26 +71,32 @@ class ApiController {
         ].findAll { k, v ->
             v
         }
-
-        header 'Access-Control-Allow-Origin', '*'
-
-        render([
-                apiVersion    : config.apiVersion ?: grailsApplication.metadata['app.version'],
-                swaggerVersion: '1.2',
-                apis          : apis,
-                info          : info
-        ] as JSON)
     }
 
-    private static Api getApi(GrailsClass controller) {
-        controller.clazz.annotations.find { ann ->
-            ann.annotationType() == Api
-        }
+    /**
+     * Obtains an Api Annotation from a controller
+     *
+     * @param controller
+     * @return
+     */
+    private Api getApi(GrailsClass controller) {
+        controller.clazz.annotations.find { it.annotationType() == Api }
+    }
+
+    /**
+     * Finds an annotation of given type in an object
+     *
+     * @param clazz
+     * @param object
+     * @return
+     */
+    private <T> T findAnnotation(Class<T> clazz, AccessibleObject object) {
+        object.annotations.find { it.annotationType() == clazz }
     }
 
     def show() {
         header 'Access-Control-Allow-Origin', '*'
-        def config = grailsApplication.config.swaggydoc
+        ConfigObject config = grailsApplication.config.swaggydoc
         def theController = grailsApplication.controllerClasses.find {
             it.logicalPropertyName == params.id
         }
@@ -70,66 +107,31 @@ class ApiController {
         def basePath = g.createLink(uri: '')
         def resourcePath = g.createLink(controller: theController.logicalPropertyName)
 
-        def apiMethods = theController.referenceInstance.class.declaredMethods.findAll { method ->
-            method.annotations.find { ann -> ann.annotationType() == ApiOperation }
-        }
+        def theControllerClazz = theController.referenceInstance.class
+        List<Method> apiMethods = theControllerClazz.declaredMethods.
+                findAll { findAnnotation(ApiOperation, it) } as List<Method>
 
-        def modelTypes = (apiMethods*.annotations.flatten().findAll {
-            ann -> ann.annotationType() == ApiOperation
-        } as List<ApiOperation>)*.response()
+        def allAnnotations = apiMethods*.annotations.flatten()
+        List<ApiOperation> apiOperationAnnotations = allAnnotations.findAll {
+            it.annotationType() == ApiOperation
+        } as List<ApiOperation>
+        def modelTypes = apiOperationAnnotations*.response()
 
-        def apis = apiMethods.collect { method ->
-            def apiParams =  (method.annotations.find {
-                it.annotationType() == ApiImplicitParams
-            } as ApiImplicitParams).value()
-            def pathParams = apiParams.findAll { Api
-                it.paramType() == 'path'
-            }.collectEntries {
-                [it.name(), "{${it.name()}}"]
-            }
-            def link = g.createLink(controller: theController.logicalPropertyName, action: method.name,
-                    params: pathParams).replace('%7B','{').replace('%7D', '}') - basePath
-            String httpMethod = theController.referenceInstance.allowedMethods[method.name] ?: 'GET'
-            ApiOperation apiOperation = method.annotations.find {
-                it.annotationType() == ApiOperation
-            } as ApiOperation
-            ApiResponses apiResponses = method.annotations.find {
-                it.annotationType() == ApiResponses
-            } as ApiResponses
-            def parameters = apiParams?.collect { apiParam ->
-                paramToMap(apiParam)
-            } ?: []
-            def inferredNickname = "${httpMethod.toLowerCase()}${theController.logicalPropertyName}${method.name}"
-            [
-                    path      : link,
-                    operations: [
-                        [
-                                method          : httpMethod,
-                                summary         : apiOperation.value(),
-                                notes           : apiOperation.notes(),
-                                nickname        : apiOperation.nickname() ?: inferredNickname,
-                                parameters      : parameters,
-                                type            : apiOperation.response() == Void ? 'void' : apiOperation.response().simpleName,
-                                responseMessages: apiResponses?.value()?.collect { apiResponse ->
-                                    [code: apiResponse.code(), message: apiResponse.message()]
-                                }
-                        ]
-                    ]
-            ]
-        }.groupBy {it.path}.collect {p, a ->
-          [path: p, operations: a*.operations.flatten() ]
-        }
+        def apis = apiMethods.
+                collect { documentMethod(it, theController) }.
+                groupBy { Map it -> it.path }.
+                collect { p, a -> [path: p, operations: (a as List<Map>).collect { it.operations }.flatten()] }
 
-        def models = modelTypes.unique().collectEntries { model ->
+        def models = modelTypes.unique().collectEntries { Class model ->
             def props = model.declaredFields.findAll {
-                !it.toString().contains(' static ') && !it.toString().contains(' transient ') && it.name != 'errors'
+                !it.toString().contains(' static ') &&
+                        !it.toString().contains(' transient ') &&
+                        it.name != 'errors'
             }
 
             def modelDescription = [
-                    id : model.simpleName,
-                    properties: props.collectEntries { Field f ->
-                        [f.name, getTypeDescriptor(f)]
-                    }
+                    id        : model.simpleName,
+                    properties: props.collectEntries { Field f -> [f.name, getTypeDescriptor(f)] }
             ]
             [model.simpleName, modelDescription]
         }
@@ -146,6 +148,54 @@ class ApiController {
         ] as JSON)
     }
 
+    private Map documentMethod(Method method, GrailsClass theController) {
+        def basePath = g.createLink(uri: '')
+        def apiOperation = findAnnotation(ApiOperation, method)
+        def apiResponses = findAnnotation(ApiResponses, method)
+        def apiParams = findAnnotation(ApiImplicitParams, method).value()
+
+        def pathParasAnnotations = apiParams.findAll { it.paramType() == 'path' } as List<ApiImplicitParam>
+        def pathParams = pathParasAnnotations*.name().collectEntries { [it, "{${it}}"] }
+
+        def slug = theController.logicalPropertyName
+
+        def fullLink = g.createLink(controller: slug, action: method.name, params: pathParams) as String
+        def link = fullLink.replace('%7B', '{').replace('%7D', '}') - basePath
+        def httpMethod = getHttpMethod(theController, method)
+        def parameters = apiParams?.collect { ApiImplicitParam it -> paramToMap(it) } ?: []
+        def inferredNickname = "${httpMethod.toLowerCase()}${slug}${method.name}"
+
+        [
+                path      : link,
+                operations: [
+                        [
+                                method          : httpMethod,
+                                summary         : apiOperation.value(),
+                                notes           : apiOperation.notes(),
+                                nickname        : apiOperation.nickname() ?: inferredNickname,
+                                parameters      : parameters,
+                                type            : apiOperation.response() == Void ? 'void' : apiOperation.response().simpleName,
+                                responseMessages: apiResponses?.value()?.collect { ApiResponse apiResponse ->
+                                    [code: apiResponse.code(), message: apiResponse.message()]
+                                }
+                        ]
+                ]
+        ]
+    }
+
+    private String getHttpMethod(GrailsClass theController, Method method) {
+        try {
+            theController.referenceInstance.allowedMethods[method.name] ?: 'GET'
+        } catch (Exception e) {
+            'GET'
+        }
+    }
+
+    /**
+     * Gets the type descriptor for a field in a domain class
+     * @param f
+     * @return
+     */
     private Map getTypeDescriptor(Field f) {
         if (f.type.isAssignableFrom(String)) {
             [type: 'string']
@@ -163,7 +213,13 @@ class ApiController {
 
     }
 
-    private LinkedHashMap<String, Serializable> paramToMap(ApiImplicitParam apiParam) {
+    /**
+     * Converts a param to a map for rendering
+     *
+     * @param apiParam
+     * @return
+     */
+    private Map paramToMap(ApiImplicitParam apiParam) {
         [
                 name       : apiParam.name(),
                 description: apiParam.value(),
