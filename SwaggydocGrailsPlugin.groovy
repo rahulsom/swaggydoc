ff04eb796a155505e146d9747d77ffbb1d4f1adb
Rahul Somasunderam <rahul.som@gmail.com>
2014-12-25 09:44:49 -0800

    Grails Dependency reduced to 2.2

--- SwaggydocGrailsPlugin.groovy
+++ SwaggydocGrailsPlugin.groovy
@@ -2,7 +2,7 @@ class SwaggydocGrailsPlugin {
     // the plugin version
     def version = "0.6-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "2.4 > *"
+    def grailsVersion = "2.2 > *"
     // resources that are excluded from plugin packaging
     def pluginExcludes = [
             "grails-app/views/error.gsp",
