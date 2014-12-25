0dacd2db165f2fc2a94d568c30cf93b73173630d
Rahul Somasunderam <rahul.som@gmail.com>
2014-12-25 09:47:03 -0800

    Bumped up next snapshot

--- SwaggydocGrailsPlugin.groovy
+++ SwaggydocGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class SwaggydocGrailsPlugin {
     // the plugin version
-    def version = "0.6"
+    def version = "0.7-SNAPSHOT"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2 > *"
     // resources that are excluded from plugin packaging
