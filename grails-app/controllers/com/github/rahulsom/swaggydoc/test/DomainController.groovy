ec30604f8b2f388533ef1054f33b7fca2e906e97
Rahul Somasunderam <rsom@certifydatasystems.com>
2015-04-19 17:38:26 -0700

    Test case for extraParams in Swaggy annotations

--- grails-app/controllers/com/github/rahulsom/swaggydoc/test/DomainController.groovy
+++ grails-app/controllers/com/github/rahulsom/swaggydoc/test/DomainController.groovy
@@ -31,84 +31,40 @@ class DomainController extends RestfulController {
         super(Domain)
     }
 
-    @Override/*
-    @ApiOperation(value = 'List demos', response = Demo, responseContainer = 'list')
-    @ApiImplicitParams([
-            @ApiImplicitParam(name = 'offset', value = 'Records to skip', defaultValue = '0', paramType = 'query', dataType = 'int'),
-            @ApiImplicitParam(name = 'max', value = 'Max records to return', defaultValue = '10', paramType = 'query', dataType = 'int'),
-            @ApiImplicitParam(name = 'sort', value = 'Field to sort by', defaultValue = 'id', paramType = 'query', dataType = 'string'),
-            @ApiImplicitParam(name = 'order', value = 'Order to sort by', defaultValue = 'asc', paramType = 'query', dataType = 'string'),
-            @ApiImplicitParam(name = 'q', value = 'Query', paramType = 'query', dataType = 'string'),
-    ])*/
-    @SwaggyList
+    @Override
+    @SwaggyList(extraParams = [
+            @ApiImplicitParam(name="include", value="Fields to include in result", dataType = "string")
+    ])
     def index() {
         super.index()
     }
 
-    @Override/*
-    @ApiOperation(value = "Show Demo", response = Demo)
-    @ApiResponses([
-            @ApiResponse(code = 400, message = 'Bad Id provided'),
-            @ApiResponse(code = 404, message = 'Could not find Demo with that Id'),
-    ])
-    @ApiImplicitParams([
-            @ApiImplicitParam(name = 'id', value = 'Id to fetch', paramType = 'path', dataType = 'int', required = true),
-    ])*/@SwaggyShow
+    @Override
+    @SwaggyShow
     def show() {
         super.show()
     }
 
-    /*@ApiOperation(value = "Save Demo", response = Demo)
-    @ApiResponses([
-            @ApiResponse(code = 422, message = 'Bad Entity Received'),
-    ])
-    @ApiImplicitParams([
-            @ApiImplicitParam(name = 'body', paramType = 'body', required = true, dataType = 'Demo'),
-    ])*/@SwaggySave
+    @SwaggySave
     @Override
     def save() {
         super.save()
     }
 
-    @Override/*
-    @ApiOperation(value = "Update Demo", response = Demo)
-    @ApiResponses([
-            @ApiResponse(code = 400, message = 'Bad Id provided'),
-            @ApiResponse(code = 404, message = 'Could not find Demo with that Id'),
-            @ApiResponse(code = 422, message = 'Bad Entity Received'),
-    ])
-    @ApiImplicitParams([
-            @ApiImplicitParam(name = 'id', value = 'Id to update', paramType = 'path', dataType = 'int', required = true),
-            @ApiImplicitParam(name = 'body', paramType = 'body', required = true, dataType = 'Demo')
-    ])*/@SwaggyUpdate
+    @Override
+    @SwaggyUpdate
     def update() {
         super.update()
     }
 
-    @Override/*
-    @ApiOperation(value = "Delete Demo")
-    @ApiResponses([
-            @ApiResponse(code = 400, message = 'Bad Id provided'),
-            @ApiResponse(code = 404, message = 'Could not find Demo with that Id'),
-    ])
-    @ApiImplicitParams([
-            @ApiImplicitParam(name = 'id', value = 'Id to delete', paramType = 'path', dataType = 'int', required = true),
-    ])*/@SwaggyDelete
+    @Override
+    @SwaggyDelete
     def delete() {
         super.delete()
     }
 
-    @Override/*
-    @ApiOperation(value = "Patch Demo", response = Demo)
-    @ApiResponses([
-            @ApiResponse(code = 400, message = 'Bad Id provided'),
-            @ApiResponse(code = 404, message = 'Could not find Demo with that Id'),
-            @ApiResponse(code = 422, message = 'Bad Entity Received'),
-    ])
-    @ApiImplicitParams([
-            @ApiImplicitParam(name = 'id', value = 'Id to patch', paramType = 'path', dataType = 'int', required = true),
-            @ApiImplicitParam(name = 'body', paramType = 'body', required = true, dataType = 'Demo')
-    ])*/@SwaggyPatch
+    @Override
+    @SwaggyPatch
     Object patch() {
         return super.patch()
     }
