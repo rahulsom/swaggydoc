ad37f1944d145ff4c1d177083a8adb2452435717
Rahul Somasunderam <rsom@certifydatasystems.com>
2015-04-20 09:46:37 -0700

    Fixing paths

--- travis.sh
+++ travis.sh
@@ -12,6 +12,17 @@ filename=$(basename $filename)
 
 echo "Publishing plugin 'swaggydoc' version $version"
 
+git clone https://${GH_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git -b gh-pages \
+    gh-pages --single-branch > /dev/null
+cd gh-pages
+git rm -rf .
+cp -r ../target/docs/. ./
+git add *
+git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
+git push origin HEAD
+cd ..
+rm -rf gh-pages
+
 if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == "rahulsom/swaggydoc" \
           && $TRAVIS_PULL_REQUEST == 'false' ]]; then
   git config --global user.name "$GIT_NAME"
@@ -24,7 +35,7 @@ if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == "rahulsom/swaggydoc" \
         gh-pages --single-branch > /dev/null
     cd gh-pages
     git rm -rf .
-    cp -r ../docs/. ./
+    cp -r ../target/docs/. ./
     git add *
     git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
     git push origin HEAD
