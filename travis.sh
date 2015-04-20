0d2c4689f1bfbc121e267f3fe12714cd65a2e97f
Rahul Somasunderam <rsom@certifydatasystems.com>
2015-04-20 09:55:10 -0700

    Setting credentials for deploy

--- travis.sh
+++ travis.sh
@@ -12,6 +12,11 @@ filename=$(basename $filename)
 
 echo "Publishing plugin 'swaggydoc' version $version"
 
+  git config --global user.name "$GIT_NAME"
+  git config --global user.email "$GIT_EMAIL"
+  git config --global credential.helper "store --file=~/.git-credentials"
+  echo "https://$GH_TOKEN:@github.com" > ~/.git-credentials
+
 git clone https://${GH_TOKEN}@github.com/$TRAVIS_REPO_SLUG.git -b gh-pages \
     gh-pages --single-branch > /dev/null
 cd gh-pages
