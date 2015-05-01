315f03206c93c03481fd413ec8198b142576b9e8
Rahul Somasunderam <rsom@certifydatasystems.com>
2015-05-01 09:23:40 -0700

    Updating version for release 0.18

--- SwaggydocGrailsPlugin.groovy
+++ SwaggydocGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class SwaggydocGrailsPlugin {
     // the plugin version
-    def version = "0.18-SNAPSHOT"
+    def version = "0.18"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "2.2 > *"
     // resources that are excluded from plugin packaging
