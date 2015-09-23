3e344a50355e76fcb2e58cca8ec2864e17cbe2a5
Rahul Somasunderam <rahul.som@gmail.com>
2015-09-22 21:31:26 -0700

    grails2 is now a subproject

--- bowerize.sh
+++ bowerize.sh
@@ -2,12 +2,23 @@
 
 bower install
 
-rm -rf web-app/images/ web-app/css/ web-app/js/
-mkdir -p web-app/images/
-mkdir -p web-app/css/
-mkdir -p web-app/js/swagger-lib/
+function resetDirs() {
+  def GRH=$1
+  if [ "$GRH" = "" ];
+    echo "No dir specified"
+    exit 1
+  fi
+  rm -rf $GRH/web-app/images/ $GRH/web-app/css/ $GRH/web-app/js/
+  mkdir -p $GRH/web-app/images/
+  mkdir -p $GRH/web-app/css/
+  mkdir -p $GRH/web-app/js/swagger-lib/
 
-cp -R bower-sources/swagger-ui/dist/images/* web-app/images/
-cp -R bower-sources/swagger-ui/dist/css/* web-app/css/
-cp -R bower-sources/swagger-ui/dist/lib/* web-app/js/swagger-lib/
-cp -R bower-sources/swagger-ui/dist/swagger-ui.js web-app/js/
+  cp -R bower-sources/swagger-ui/dist/images/*      $GRH/web-app/images/
+  cp -R bower-sources/swagger-ui/dist/css/*         $GRH/web-app/css/
+  cp -R bower-sources/swagger-ui/dist/lib/*         $GRH/web-app/js/swagger-lib/
+  cp -R bower-sources/swagger-ui/dist/swagger-ui.js $GRH/web-app/js/
+
+}
+
+resetDirs grails2
+resetDirs grails3
