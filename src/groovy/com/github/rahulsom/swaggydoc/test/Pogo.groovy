a5c325eb49b5de0c21c0375e0b2e7c0d483f02ef
Sharim Chua <sharim@socketsoftware.com>
2015-10-28 14:53:34 +1000

    Support fields that have collection types which are self referencing on POGOs

--- src/groovy/com/github/rahulsom/swaggydoc/test/Pogo.groovy
+++ src/groovy/com/github/rahulsom/swaggydoc/test/Pogo.groovy
@@ -3,7 +3,8 @@ package com.github.rahulsom.swaggydoc.test
 /**
  * Created by rahulsomasunderam on 11/02/15.
  */
-class Pogo {
-    Integer id
+ class Pogo {
+    Integer id    
+    List<Pogo> siblingPogoList
     List<SubPogo> subPogoList
 }
