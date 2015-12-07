b68c32111aae276ab99d46af14d6f35408634703
Rahul Somasunderam <rahul.som@gmail.com>
2015-12-06 19:53:05 -0800

    Fix dir name for doc upload

--- travis.sh
+++ travis.sh
@@ -22,7 +22,7 @@ if [[ $TRAVIS_BRANCH == 'master' && $TRAVIS_REPO_SLUG == "rahulsom/swaggydoc" \
     cd gh-pages
     git rm -rf .
     mkdir grails2
-    cp -r ../grails2/target/docs/. ./grails2/
+    cp -r ../grails2/build/docs/. .
     git add *
     git commit -a -m "Updating docs for Travis build: https://travis-ci.org/$TRAVIS_REPO_SLUG/builds/$TRAVIS_BUILD_ID"
     git push origin HEAD
